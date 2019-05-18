d74abbf3f1e3da8f4a0ef9c4c2ac83ab9697f81f
Evan Sosenko <razorx@evansosenko.com>
2019-05-18 11:02:01 -0700

    1.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='makenew-serverless-python',
-    version='1.0.2',
+    version='1.0.3',
     author='Evan Sosenko',
     author_email='razorx@evansosenko.com',
     packages=find_packages(exclude=['docs']),
